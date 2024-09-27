aa9527a3f04663ffe8f73b62cfdf78e08b903fb3
amchvear <161626020+amchvear@users.noreply.github.com>
2024-09-27 21:46:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const sections = document.querySelectorAll('.section');
 
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const viewportHeight = window.innerHeight;
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition >= sectionTop - viewportHeight / 2 && scrollPosition < sectionTop + sectionHeight - viewportHeight / 2) {
+            section.classList.add('show');
+        } else {
+            section.classList.remove('show');
+        }
+    });
+});
